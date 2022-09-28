88f350d8fe9e77b614fd10e896d81dec32fc1482
grilgamesh <grilly@gmail.com>
2022-09-28 10:55:35 +0100

    added navbar

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 function guessed(guessRaw) {
     //replace display with selected value
     console.log("checkpoint: guess made: " + guessRaw);
-    // guess = SPAG_remover(guessRaw);
+    guess = SPAG_remover(guessRaw);
 
-    if (guessRaw == answerKey){
+    if (guess == answerKey){
         guess_counter++;
         let link = "https://www.imdb.com/title/" + answerDict['id'];
         if (megahint_revealed == false){
@@ -39,8 +39,8 @@ function guessed(guessRaw) {
         x.style.visibility = "hidden";
     }    
     else{
-        for (i=0; i<autocompleter.length; i++){
-            if (guessRaw == autocompleter[i]){
+        for (i=0; i<films.length; i++){
+            if (guess == year_remover(films[i])){
                 var guess_Dict = film_dict[films[i]];
                 var guess_tags = guess_Dict['tags'];
                 if (guess_list.includes(guess_Dict['punc_name'])){
@@ -209,105 +209,6 @@ function guessed(guessRaw) {
 
 }
 
-// Standard autocomplete code
-function autocomplete(inp, arr) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
-    var currentFocus;
-    /*execute a function when someone writes in the text field:*/
-    inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/
-        closeAllLists();
-        if (!val) { return false;}
-        currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
-        /*for each item in the array...*/
-        for (i = 0; i < arr.length; i++) {
-          /*check if the item starts with the same letters as the text field value:*/
-          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-            /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function(e) {
-                /*insert the value for the autocomplete text field:*/
-                inp.value = this.getElementsByTagName("input")[0].value;
-                /*close the list of autocompleted values,
-                (or any other open lists of autocompleted values:*/
-                closeAllLists();
-            });
-            a.appendChild(b);
-          }
-        }
-    });
-    /*execute a function presses a key on the keyboard:*/
-    inp.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-          /*If the arrow DOWN key is pressed,
-          increase the currentFocus variable:*/
-          currentFocus++;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 38) { //up
-          /*If the arrow UP key is pressed,
-          decrease the currentFocus variable:*/
-          currentFocus--;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 13) {
-          /*If the ENTER key is pressed, prevent the form from being submitted,*/
-          e.preventDefault();
-          if (currentFocus > -1) {
-            /*and simulate a click on the "active" item:*/
-            if (x) x[currentFocus].click();
-          }
-        }
-    });
-    function addActive(x) {
-      /*a function to classify an item as "active":*/
-      if (!x) return false;
-      /*start by removing the "active" class on all items:*/
-      removeActive(x);
-      if (currentFocus >= x.length) currentFocus = 0;
-      if (currentFocus < 0) currentFocus = (x.length - 1);
-      /*add class "autocomplete-active":*/
-      x[currentFocus].classList.add("autocomplete-active");
-    }
-    function removeActive(x) {
-      /*a function to remove the "active" class from all autocomplete items:*/
-      for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("autocomplete-active");
-      }
-    }
-    function closeAllLists(elmnt) {
-      /*close all autocomplete lists in the document,
-      except the one passed as an argument:*/
-      var x = document.getElementsByClassName("autocomplete-items");
-      for (var i = 0; i < x.length; i++) {
-        if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
-  }
-  /*execute a function when someone clicks in the document:*/
-  document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
-  });
-  } 
-
-
 function format_keywords(tags){
     var width = 4;
     var html = "";
@@ -329,17 +230,17 @@ function hint(){
     // guess a random film
     randHint = getAnswer();
     // remove confusing punctuation and forgettable words from the hint
-    // randHint = year_remover(randHint);
-    // hintKey = SPAG_remover(randHint);
+    randHint = year_remover(randHint);
+    hintKey = SPAG_remover(randHint);
     // make sure the hint is not the solution
-    while (randHint == answerKey){
+    while (hintKey == answerKey){
         randHint = getAnswer();
-        // randHint = year_remover(randHint);
-        // hintKey = SPAG_remover(randHint);
+        randHint = year_remover(randHint);
+        hintKey = SPAG_remover(randHint);
     }
 
     // guess the hint
-    guessed(film_dict[randHint]['punc_name']);
+    guessed(hintKey);
     hint_counter++;
 
     // reveal the megahint function once this has been used three times
@@ -461,13 +362,12 @@ function init(){
     randKey = getAnswer();
     console.log(randKey);
     // remove year from the answerKey
-    // answerKey = year_remover(randKey);
+    answerKey = year_remover(randKey);
+    console.log(answerKey);
     // Use the key to get the corresponding name from the "names" object
     answerDict = film_dict[randKey];
     console.log(answerDict);
     answer_tags = answerDict['tags'];
-    answerKey = answerDict['punc_name'];
-    console.log(answerKey);
 
     // set up blank spaces
     d3.select('#filmNotFound').html("<p>film_dict loaded. there are " + films.length + " entries in solution set. Type a film and hit enter or click guess...</p>");
@@ -510,24 +410,16 @@ let list_of_guesses = '';
 let hint_counter = 0;
 let megahint_revealed = false;
 
-var autocompleter = [];
 
-
-var url_json = "https://grilgamesh.github.io/Taggle/data/imdb_tag_game_100.json";
+var url20 = "https://grilgamesh.github.io/Taggle/data/imdb_tag_game_100.json";
 console.log("please wait while data loads");
 d3.select('#filmNotFound').html("<p>please wait while data loads</p>");
 
-d3.json(url_json).then(function(response) {
+d3.json(url20).then(function(response) {
     film_dict = response;
     films = Object.keys(film_dict)
     console.log("film_dict loaded. there are " + films.length + " entries in solution set");
     console.log(films);
-
-    for (i = 0; i<films.length; i++){
-        autocompleter.push(film_dict[films[i]]['punc_name']);
-    }
-    autocomplete(document.getElementById("guessInput"), autocompleter);
-    console.log(autocompleter);
 
     init();
 })
